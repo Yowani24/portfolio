bfa17c78700c0eefcb38effe664a9e291d00210a
Yowani24 <yowani24@outlook.com>
2021-11-23 23:27:51 -0300

    new change5

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,23 @@ import Image from 'next/image'
 const webdevImaage = require('../public/webdev.png');
 const profImage = require('../public/userImage.png');
 
+import { PDFDownloadLink, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 import { FiMenu } from 'react-icons/fi';
 import { IoIosArrowDown } from 'react-icons/io';
 import { FaLinkedinIn, FaInstagram, FaFacebookF, FaTwitter, FaUserCircle, FaUser } from 'react-icons/fa';
+
+const MyDoc = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Iso é apenas um este</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>It is just a test</Text>
+      </View>
+    </Page>
+  </Document>
+);
 
 export default function Home() {
   return (
@@ -50,7 +64,11 @@ export default function Home() {
               <p className={styles.details}>Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.</p>
               <div className={styles.btns_box}>
                 <button className={styles.portfolio_btn}>Portfolio</button>
-                <button className={styles.resume_btn}>Download Resume</button>
+                <button className={styles.resume_btn}>
+                  <PDFDownloadLink document={<MyDoc />} fileName="Joao_da_Costa.pdf">
+                    {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download Resume')}
+                  </PDFDownloadLink>
+                </button>
               </div>
 
               <div className={styles.icons}>
@@ -59,6 +77,7 @@ export default function Home() {
                 <div className={styles.icon_box}><FaTwitter/></div>
                 <div className={styles.icon_box}><FaFacebookF/></div>
               </div>
+
             </div>
 
             <div className={styles.topSide_right}>
